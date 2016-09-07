9ca1e392dad695ef196fdab5ab19f14e6a14eec7
Zachariah Moreno <xachmoreno@gmail.com>
2016-09-07 14:36:22 -0700

    Revert "Defined controllers module as dependency of root app (#14)"
    This reverts commit cab95e80b7d122de127069aaa26048fb4c126bc4.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
-(function() {
-  'use strict';
-
-  angular.module('code', ['ngRoute', 'controllers', 'services']).
-
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-    when('/', {templateUrl: 'partials/main.html',   controller: 'CodeCtrl'}).
-    when('/guide', {templateUrl: 'partials/guide.html', controller: 'GuideCtrl'}).
-    when('/SRA', {templateUrl: 'partials/guide.html', controller: 'GuideCtrl'}).
-    when('/share', {templateUrl: 'partials/share.html'}).
-    otherwise({redirectTo: '/'});
-  }]);
-})();
+'use strict';
+angular.module('code', ['ngRoute', 'services']).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+  when('/', {templateUrl: 'partials/main.html',   controller: CodeCtrl}).
+  when('/guide', {templateUrl: 'partials/guide.html', controller: GuideCtrl}).
+  when('/SRA', {templateUrl: 'partials/guide.html', controller: GuideCtrl}).
+  when('/share', {templateUrl: 'partials/share.html'}).
+  otherwise({redirectTo: '/'});
+}]
+);
