fea0c8543776a475ec674d6aa58c0750509a7f4b
Keanu Lee <keanulee@google.com>
2016-11-07 15:01:54 -0800

    Bunch of fixes

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -11,7 +11,9 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/**/*',
+    '/data/*'
   ],
   navigateFallback: '/index.html'
 };
